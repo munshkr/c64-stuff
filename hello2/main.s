6f17611b8a1ba9540c94b9614cb257795b534cef
Damián Silvani <munshkr@gmail.com>
2015-07-19 21:20:30 -0300

    hello2: Fix comment on autostart routine

--- hello2/main.s
+++ hello2/main.s
@@ -4,7 +4,7 @@
 
 ; BASIC autostart routine (aka "10 SYS 4096")
 .byte $0c, $08, $0a, $00, $9e, $20
-.byte $34, $39, $31, $35, $32            ; 4096 = $1000
+.byte $34, $39, $31, $35, $32            ; 49152 = $c000
 .byte $00, $00, $00
 
 .dsb $c000 - *  ; pad with zeroes from PC to $c000
