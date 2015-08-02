3ffbcbb6e2bd9687dcb26caea7102d8360443dd7
Damián Silvani <munshkr@gmail.com>
2015-08-01 21:02:59 -0300

    ball: Move ball sprite with acceleration

---
+++ ball/main.s
@@ -0,0 +1,162 @@
+.import __MAIN_CODE_LOAD__
+
+.include "c64.inc"  ; C64 constants
+
+SCREEN_WIDTH = 320
+SCREEN_HEIGHT = 200
+SCREEN_RIGHT_BORDER_WIDTH = 24
+SCREEN_TOP_BORDER_HEIGHT = 30
+SPRITE_HALF_WIDTH = 6
+SPRITE_HALF_HEIGHT = 10
+CENTER_X = ((SCREEN_WIDTH / 2) + SCREEN_RIGHT_BORDER_WIDTH - SPRITE_HALF_WIDTH)
+CENTER_Y = ((SCREEN_HEIGHT / 2) + SCREEN_TOP_BORDER_HEIGHT - SPRITE_HALF_HEIGHT)
+
+CHAR_BACKGROUND_COLOR = $00
+CHAR_COLOR = $01
+
+
+.segment "DATA"
+    pos_x:   .byte CENTER_X
+    pos_y:   .byte CENTER_Y
+    pos_x_h: .byte 0
+    vel_x:   .byte 1
+    vel_y:   .byte 0
+    accel_x: .byte 1
+    accel_y: .byte 0
+
+.segment "CODE"
+    jmp __MAIN_CODE_LOAD__
+
+.segment "MAIN_CODE"
+    sei
+
+    jsr init_screen   ; clear the screen
+    jsr init_sprite   ; enable sprite
+
+    lda #%01111111
+    sta CIA1_ICR      ; turn off CIAs Timer interrupts
+    sta CIA2_ICR
+    lda CIA1_ICR      ; by reading $dc0d and $dd0d we cancel all CIA-IRQs
+    lda CIA2_ICR      ; in queue/unprocessed.
+
+    lda #$01          ; set Interrupt Request Mask
+    sta VIC_IMR       ; we want IRQ by Rasterbeam (%00000001)
+
+    lda #<irq         ; point IRQ Vector to our custom irq routine
+    ldx #>irq
+    sta IRQVec        ; store in $314/$315
+    stx IRQVec+1
+
+    lda #0            ; trigger interrupt at row zero
+    sta VIC_HLINE
+
+    cli
+    jmp *
+
+irq:
+    dec VIC_IRR       ; acknowledge IRQ / clear register for next interrupt
+
+    jsr update_velocity
+    jsr update_positions
+    jsr update_sprites
+
+    jmp $ea31       ; return to Kernel routine
+
+init_screen:
+    ldx #0
+    stx VIC_BG_COLOR0     ; set background color
+    stx VIC_BORDERCOLOR   ; set border color
+@loop:
+    lda #$20      ; #$20 is the spacebar Screen Code
+    ; TODO: Use constant SCREEN_RAM + offset
+    sta $0400, x  ; fill four areas with 256 spacebar characters
+    sta $0500, x
+    sta $0600, x
+    sta $06e8, x
+
+    lda #$01      ; set foreground to black in Color RAM
+    ; TODO: Use constant SCREEN_RAM + offset
+    sta $d800, x
+    sta $d900, x
+    sta $da00, x
+    sta $dae8, x
+
+    inx
+    bne @loop
+    rts
+
+init_sprite:
+    lda #%00000001  ; enable sprite #0
+    sta VIC_SPR_ENA
+
+    lda #0          ; clear multicolor mode for all sprites
+    sta VIC_SPR_MCOLOR
+
+    lda #0          ; all sprites have priority over background
+    sta VIC_SPR_BG_PRIO
+
+    ; set shared colors
+    lda #CHAR_BACKGROUND_COLOR
+    sta VIC_BG_COLOR0
+
+    ; set sprite #0 color
+    lda #CHAR_COLOR
+    sta VIC_SPR0_COLOR
+
+    ; set sprite #0 image
+    lda #$80
+    sta $07f8
+
+    rts
+
+.proc update_velocity
+    clc
+    lda accel_x
+    adc vel_x
+    sta vel_x
+    clc
+    lda accel_y
+    adc vel_y
+    sta vel_y
+    rts
+.endproc
+
+.proc update_positions
+    clc
+    lda vel_y
+    adc pos_y
+    sta pos_y
+
+    clc
+    lda vel_x
+    bpl @pos
+@neg:
+    adc pos_x
+    sta pos_x
+    bcs @done
+    beq @toggle_pos_x_h
+@pos:
+    adc pos_x
+    sta pos_x
+    bcc @done
+@toggle_pos_x_h:
+    lda pos_x_h
+    eor #%00000001
+    sta pos_x_h
+@done:
+    rts
+.endproc
+
+.proc update_sprites
+    lda pos_x
+    sta VIC_SPR0_X
+    lda pos_y
+    sta VIC_SPR0_Y
+    lda pos_x_h          ; update bit#8 of x-coordinate of all sprites
+    sta VIC_SPR_HI_X
+    rts
+.endproc
+
+
+.segment "SPRITES"
+    .incbin "ball.raw"
